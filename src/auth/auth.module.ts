dd28be26b9cd5e41cff7feb6eebd50fda45ca504
Mohamed Jaidane <jaidanemohammed@gmail.com>
2023-01-20 15:03:18 +0100

    add user authentication and authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import 'dotenv/config'
 
-@Module({})
+@Module({
+  imports: [
+    UserModule, 
+    PassportModule,     
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  providers: [
+    AuthService, 
+    LocalStrategy, 
+    JwtStrategy
+  ],
+  controllers: [AuthController],
+})
 export class AuthModule {}
